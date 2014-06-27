1b3a6f54c369adcf0c1941ace546eb25ef818284
Anna Neyzberg <aneyzberg@gmail.com>
2014-06-27 03:49:01 +0000

    Destroyed topics, posts and comments.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
 
+
   default_scope { order('created_at DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
