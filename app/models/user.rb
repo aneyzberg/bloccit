931edbda694590bdaa8ce70faaf2c764fdf2f8b5
Anna Neyzberg <aneyzberg@gmail.com>
2014-06-29 02:58:06 +0000

    Implemented favoriting

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,13 @@
   has_many :posts
   has_many :comments
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
 mount_uploader :avatar, AvatarUploader
 
-
+ def favorited(post)
+    self.favorites.where(post_id: post.id).first
+  end
 
   def role?(base_role)
   role == base_role.to_s
